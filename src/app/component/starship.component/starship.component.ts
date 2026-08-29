f9bfd9e7d55642acb1b0345923ffae076e896d00
KristanDharel <dharelkristan@gmail.com>
2026-08-29 17:08:15 +0545

    File Structure Setup

---
+++ src/app/component/starship.component/starship.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-starship.component',
+  imports: [],
+  templateUrl: './starship.component.html',
+  styleUrl: './starship.component.css',
+})
+export class StarshipComponent {}
